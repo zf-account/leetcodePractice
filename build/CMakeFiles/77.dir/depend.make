88274f264f233dafbd4be0b9d2866b5e5cb24874
zf-account <78484310+zf-account@users.noreply.github.com>
2024-09-24 19:40:24 +0800

    update

--- build/CMakeFiles/77.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for 77.
-# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/77ZuHe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "77ZuHe"
+  "77ZuHe.pdb"
+  "CMakeFiles/77ZuHe.dir/src/77ZuHe.cpp.o"
+  "CMakeFiles/77ZuHe.dir/src/77ZuHe.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/77ZuHe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

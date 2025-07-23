560ead9fd515f657e63a338a3104f0c5a49ca1b3
zf-account <2838452702@qq.com>
2025-07-23 22:22:40 +0800

    refresh 704

--- src/leetcode/704ErFenChaZhao.cpp
+++ src/leetcode/704ErFenChaZhao.cpp
@@ -3,23 +3,6 @@
 #include <vector>
 
 using namespace std;
-/* --------------------------------------------------- 左闭右闭写法 --------------------------------------------- */
-// class Solution {
-// public:
-//     int search(vector<int>& nums, int target) {
-//         int left = 0, right = nums.size() - 1;
-//         while (left <= right) { //注意为什么是小于等于
-//             int mid = (left + right) / 2;
-//             if (nums[mid] > target) {
-//                 right = mid - 1; //注意为什么是mid-1
-//             } else if (nums[mid] < target) {
-//                 left = mid + 1; //注意为什么是mid+1
-//             } else
-//                 return mid;
-//         }
-//         return -1;
-//     }
-// };
 /* --------------------------------------------------- 左闭右开写法 --------------------------------------------- */
 class Solution {
 public:
@@ -39,6 +22,10 @@ public:
 };
 int main(int argc, char const *argv[])
 {
-    
+    Solution s;
+    vector<int> nums = {-1, 0, 3, 5, 9, 12};
+    int target = 9;
+    int result = s.search(nums, target);
+    cout << result << endl;
     return 0;
 }
